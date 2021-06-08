b4be715576842206ca771f4de9421409a3df14ba
Krishnamurthy <vikrishnamur@P2LP5CD80884P2.corp.checkfree.com>
2021-06-08 19:47:11 -0400

    added link to contacts page

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,15 @@
 //Home page Code
+import { Fragment } from 'react';
 
 function HomePage() {
-    return <h1>Welcome to Contacts</h1>
+    return (
+        <Fragment>
+            <h1>Welcome to Contacts!</h1>
+            <h2>
+                <a href="/contacts">NEW CONTACT LIST</a>  //takes you to contacts page
+            </h2>
+        </Fragment>
+    );
 }
 
 export default HomePage;
