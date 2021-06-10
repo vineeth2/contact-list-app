f657790704e995f4ce0d9eebc3170d4d98d1b850
Krishnamurthy <vikrishnamur@che-feb21_2.corp.checkfree.com>
2021-06-10 16:21:06 -0400

    Added functionality to adding a new contact

--- components/ContactList.js
+++ components/ContactList.js
@@ -4,15 +4,16 @@ import classes from './ContactList.module.css'
 function ContactList(props) {
     return (
         <ul className={classes.list}>
-            {props.contacts.map((contact) => (
-                <Contact
-                    name={contact.name}
-                    email={contact.email}
-                    address={contact.address}
-                    number={contact.number}
-                    id={contact.id}
-                />
-            ))}
+        {props.contacts.map((contact) => (
+            <Contact
+            fname={contact.fname}
+            lname={contact.lname}
+            email={contact.email}
+            phone={contact.phone}
+            address={contact.address}
+            id={contact.id}
+            />
+        ))}
         </ul>
     )
 }
