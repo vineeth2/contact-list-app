210144e531b3e6a1bd341c69eef1e448967c8313
Krishnamurthy <vikrishnamur@ALPHX-VKRISH.local>
2021-06-16 14:19:25 -0400

    added features for phone number

--- pages/add-contact.js
+++ pages/add-contact.js
@@ -45,7 +45,6 @@ function addContactPage() {
             </Head>
             <h1>Add a new contact below</h1>
             <NewContactForm onAddContact={addContactHandler} />
-            <Link href='/'>Back to home</Link>
         </Fragment>
     );
 }
